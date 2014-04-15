2afd8d3283925856cb801e365f66dffc5589567a
Mel <woodgettm@gmail.com>
2014-04-14 20:24:11 -0400

    Worked up to render() shortcut.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,29 @@
 from django.http import HttpResponse
+from polls.models import Poll
+#from django.template import RequestContext, loader
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the poll index.")
+    #return HttpResponse("Hello, world. You're at the poll index.")
+
+    """ 
+    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+    output = ', '.join([p.question for p in latest_poll_list])
+    return HttpResponse(output)
+    """
+
+    """
+    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = RequestContext(request, {
+        'latest_poll_list' : latest_poll_list,
+    })
+    return HttpResponse(template.render(context))
+    """
+
+    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+    context = {'latest_poll_list': latest_poll_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, poll_id):
     return HttpResponse("You're looking at the poll %s." % poll_id)
