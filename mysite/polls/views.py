9cdf277679731d4c7e0c616ebc5c283d4f781e57
mello151 <woodgettm@gmail.com>
2014-04-14 23:41:23 -0400

    Up to tutorial 4

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from polls.models import Poll
 #from django.template import RequestContext, loader
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from polls.models import Poll
 
 def index(request):
     #return HttpResponse("Hello, world. You're at the poll index.")
@@ -26,7 +27,16 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, poll_id):
-    return HttpResponse("You're looking at the poll %s." % poll_id)
+    #return HttpResponse("You're looking at the poll %s." % poll_id)
+    """
+    try:
+        poll = Poll.objects.get(pk=poll_id)
+    except Poll.DoesNotExist:
+        raise Http404
+    return render(request, 'polls/detail.html', {'poll': poll})
+    """
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/detail.html', {'poll': poll})
 
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s." % poll_id)
