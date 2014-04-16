8acca7e18637cba4cf7c09fd470d4746454e3956
mello151 <woodgettm@gmail.com>
2014-04-15 23:39:35 -0400

    Up to 'class PollViewTests'

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
+import datetime
+from django.utils import timezone
+from polls.models import Poll
+from django.core.urlresolvers import reverse
 
 # Create your tests here.
+
+class PollMethodTests(TestCase):
+
+    def test_was_published_recently_with_future_poll(self):
+        """
+        was published_recently() should return False for polls whose 
+        pub_date is in the future
+        """
+        future_poll = Poll(pub_date=timezone.now() + datetime.timedelta(days=30))
+        self.assertEqual(future_poll.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_poll(self):
+        """
+        was_published_recently() should return True for polls whose 
+        pub_date is within the last day
+        """
+        old_poll = Poll(pub_date=timezone.now() - datetime.timedelta(days=30))
+        self.assertEqual(old_poll.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_poll(self):
+        """
+        was_published_recently() should return True for polls whose
+        pub_date is within the last day
+        """
+        recent_poll = Poll(pub_date=timezone.now() - datetime.timedelta(hours=1))
+        self.assertEqual(recent_poll.was_published_recently(), True)
+
+    def create_poll(question, days):
+        """
+        Creates a poll with the given 'question' published the given
+        number of 'days' offset to now (negative for polls published
+        in the past, positive for polls that have yet to be published).
+        """
+        return Poll.objects.create(question=question,
+            pub_date=timezone.now() + datetime.timedelta(days=days))
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,31 @@
-from django.http import HttpResponse, HttpResponseRedirect
+#from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from polls.models import Poll, Choice
 from django.shortcuts import render, get_object_or_404
 from django.core.urlresolvers import reverse
+from django.views import generic
+from django.utils import timezone
 
-def index(request):
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_poll_list'
+
+    def get_queryset(self):
+        """ Return the last five published polls."""
+        #return Poll.objects.order_by('-pub_date')[:5]
+        return Poll.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/results.html'
+
+#def index(request):
     #return HttpResponse("Hello, world. You're at the poll index.")
 
     """ 
@@ -21,11 +43,13 @@ def index(request):
     return HttpResponse(template.render(context))
     """
 
+    """
     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
     context = {'latest_poll_list': latest_poll_list}
     return render(request, 'polls/index.html', context)
+    """
 
-def detail(request, poll_id):
+#def detail(request, poll_id):
     #return HttpResponse("You're looking at the poll %s." % poll_id)
     """
     try:
@@ -34,13 +58,18 @@ def detail(request, poll_id):
         raise Http404
     return render(request, 'polls/detail.html', {'poll': poll})
     """
+
+    """
     poll = get_object_or_404(Poll, pk=poll_id)
     return render(request, 'polls/detail.html', {'poll': poll})
+    """
 
-def results(request, poll_id):
+#def results(request, poll_id):
     #return HttpResponse("You're looking at the results of poll %s." % poll_id)
+    """
     poll = get_object_or_404(Poll, pk=poll_id)
     return render(request, 'polls/results.html', {'poll': poll})
+    """
 
 def vote(request, poll_id):
     #return HttpResponse("You're voting on poll %s." % poll_id)
